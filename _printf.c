ea6aaa4f97892a94cf38139fc04dd3d106726e98
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-09-18 15:19:39 +0200

    refactoring printf

--- _printf.c
+++ _printf.c
@@ -4,6 +4,7 @@
   * @format: character string with format if any
   * Return: number of characters printed
   */
+int _printf(const char *format, ...);
 int _printf(const char *format, ...)
 {
 	int printed_chars = 0;
